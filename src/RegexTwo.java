f03ea003f64b2e451f791754595686576269d500
IhorKov <apasniy1234@gmail.com>
2021-07-10 22:05:54 +0300

    fix

--- src/RegexTwo.java
+++ src/RegexTwo.java
@@ -4,25 +4,25 @@ import java.util.regex.Pattern;
 public class RegexTwo {
     public static void main(String[] args) {
         String mail = "apasniy1234@gmail.com";
-        String mailError = "afdasf";
+        String mailError = "@abc.def";
 
         printMail(mail);
         printMail(mailError);
     }
 
     public static void printMail(String str){
-
         String result;
-        String regex = "@.*\\..*";
+        String regex = ".(@.*\\..*)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(str);
 
         if(matcher.find()){
-            result = matcher.group();
+            result = matcher.group(1);
             System.out.println(result);
         }
         else {
             System.out.println("!совпадений не найдено!");
         }
     }
+    
 }
